2833e59dc3b9b88853c6948b65cf58a021c97111
Zeynep Bayraktar <zeynepbayraktar2006@gmail.com>
2026-01-31 17:13:02 +0300

    day6: forearch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace marketAutomation
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace marketAutomation
 {
     internal class Program
     {
@@ -197,6 +199,91 @@
 
             #region day4 Receipt Management with Arrays
 
+            // Console.WriteLine("---WELCOME TO MY MARKET---");
+            // Console.WriteLine("--------------------------");
+
+            // Console.Write("How many items will you buy? ");
+            // int size = int.Parse(Console.ReadLine());
+
+            // string[] items = new string[size];
+            // double[] prices = new double[size];
+            // int[] count = new int[size];
+            // double totalPrice = 0;
+
+            // for (int i = 0; i < size; i++)
+            // {
+            //     Console.Write($"\n{i + 1}. Product Entry: ");
+
+            //     Console.Write("Product name: ");
+            //     items[i] = Console.ReadLine();
+    
+            //     Console.Write("Unit Price: ");
+            //     prices[i] = double.Parse(Console.ReadLine());
+
+            //     Console.Write("Quantity: ");
+            //     count[i] = int.Parse(Console.ReadLine());
+
+            //     totalPrice += count[i]*prices[i];
+
+                
+            // }
+
+            // double discountedPrice=0;
+            // double tax=0;
+            // double lastPrice=0;
+            // if (totalPrice >= 500)
+            // {
+            //     discountedPrice = totalPrice*0.90;
+            //     tax = discountedPrice * 0.20;
+            //     lastPrice = discountedPrice + tax;
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Reach 500 TL and get a 10% discount on your next purchase!");
+            //         tax = totalPrice * 0.20;
+            //         lastPrice = totalPrice + tax;
+            // }
+
+            // Console.Write("Press 1 for cash, 2 for credit card: ");
+            // int paymentMethod = int.Parse(Console.ReadLine() ?? "2");
+            // if (paymentMethod == 1) 
+            // {
+            //     Console.WriteLine("Cash payment selected. Extra 5 TL discount is applied.");
+            //     lastPrice = lastPrice - 5;
+            // }
+            // else if (paymentMethod == 2)
+            // {
+            //     Console.WriteLine("Payment by credit card was selected.");
+            // }
+            // else 
+            // {
+            //     Console.WriteLine("You made an invalid selection; the payment was processed as a credit card.");
+            // }
+            
+            // Console.WriteLine("\n------- MARKET RECEIPT -------");
+            // for (int j = 0; j < size; j++)
+            // {
+            //     double itemTotal = prices[j] * count[j];
+            //     Console.WriteLine($"{items[j]}\t x{count[j]}\t : {itemTotal:0.00} TL");
+            // }
+
+            // Console.WriteLine("---------------------------");
+            // if(discountedPrice == 0)
+            // {
+            //     Console.WriteLine($"Subtotal\t: {totalPrice:0.00} TL");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"Subtotal\t: {discountedPrice:0.00} TL");
+            // }
+            // Console.WriteLine($"Tax (20%)\t: {tax:0.00} TL");
+            // Console.WriteLine($"GRAND TOTAL\t: {lastPrice:0.00} TL");
+            // Console.WriteLine("--------------------------");
+
+            #endregion
+
+            #region day5 Foreach ile Market Analizi
+
             Console.WriteLine("---WELCOME TO MY MARKET---");
             Console.WriteLine("--------------------------");
 
@@ -208,7 +295,7 @@
             int[] count = new int[size];
             double totalPrice = 0;
 
-            for (int i = 0; i < size; i++)
+            foreach (int i in Enumerable.Range(0, size))
             {
                 Console.Write($"\n{i + 1}. Product Entry: ");
 
@@ -222,8 +309,16 @@
                 count[i] = int.Parse(Console.ReadLine());
 
                 totalPrice += count[i]*prices[i];
-
                 
+            }
+
+            double maxPrice = 0;
+            string maxPriceProduct = "";
+            foreach(int j in Enumerable.Range(0, prices.Length)){
+                if(prices[j] > maxPrice){
+                    maxPrice = prices[j];
+                    maxPriceProduct = items[j];
+                }
             }
 
             double discountedPrice=0;
@@ -259,12 +354,28 @@
             }
             
             Console.WriteLine("\n------- MARKET RECEIPT -------");
-            for (int j = 0; j < size; j++)
+            foreach(int i in Enumerable.Range(0, items.Length))
             {
-                double itemTotal = prices[j] * count[j];
-                Console.WriteLine($"{items[j]}\t x{count[j]}\t : {itemTotal:0.00} TL");
+                double itemTotal = prices[i] * count[i];
+                Console.WriteLine($"{items[i]}\t x{count[i]}\t : {itemTotal:0.00} TL");
             }
 
+            int k = 0;
+            foreach (string item in items)
+            {
+                if (prices[k] > 100)
+                {
+                     Console.WriteLine($"{item} - {prices[k]} TL (Lüks Ürün)");
+                }
+                else
+                {
+                Console.WriteLine($"{item} - {prices[k]} TL");
+                }
+                k++;
+            }
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Your maximum purchase is {maxPriceProduct}: {maxPrice:0.00} TL");
             Console.WriteLine("---------------------------");
             if(discountedPrice == 0)
             {
--- obj/Debug/net10.0/marketAutomation.AssemblyInfo.cs
+++ obj/Debug/net10.0/marketAutomation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("marketAutomation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd33985cd6763d73832c464c9369be1ea453995")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc2631e844321becdb76ca0d63236eb11e6206e")]
 [assembly: System.Reflection.AssemblyProductAttribute("marketAutomation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("marketAutomation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
